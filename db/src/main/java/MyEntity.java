0128b16f5a40736e5a2972c13c4e746babfc71d3
Vladimir Troshin <vtroshin@rvision.pro>
2022-05-23 21:25:24 +0300

    Initial commit

---
+++ db/src/main/java/MyEntity.java
@@ -0,0 +1,36 @@
+import java.util.UUID;
+
+/**
+ * @author Vladimir Troshin on 23.05.2022
+ * https://jira.rvision.pro/browse/
+ */
+public class MyEntity {
+
+    private UUID id;
+    private String name;
+
+    public MyEntity(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append("MyEntity{").append("id=").append(id).append(", name='").append(name).append('\'').append('}').toString();
+    }
+}
